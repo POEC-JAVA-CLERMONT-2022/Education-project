01125aa347dfb655da6bf868d5c4ad9f9b0cd200
Utilisateur <soubla19@hotmail.fr>
2022-04-01 10:07:31 +0200

    Modif sur Lesson lessonRepo lessonService ModuleService

--- src/main/java/com/educ/data/LessonRepository.java
+++ src/main/java/com/educ/data/LessonRepository.java
@@ -1,5 +1,7 @@
 package com.educ.data;
 
+import com.educ.entity.Language;
+import com.educ.entity.Level;
 import com.educ.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -12,7 +14,10 @@ public interface LessonRepository extends JpaRepository<Lesson, Long>  {
     //@Query("select b from Book b where b.name = :userName")
     public Lesson getById(Long id);
 
-    @Query("select l from Lesson l where l.name = :name")
-    Lesson findByName(@Param("name")String name);
+    /* @Query("select l from Lesson l where l.name = :name")
+    Lesson findByName(@Param("name")String name);  */
+
+    @Query("select l from Lesson l where l.name = :name and l.level= :level, l.language= :language")
+    Lesson findByNameAndLevelAndLanguage(@Param("name")String name, @Param("level") Level level, @Param("language") Language language);
 
 }
